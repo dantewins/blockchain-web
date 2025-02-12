72aacac8f12db5b8495ba68e693a666873805539
Azurium-js <kimdanny0603@gmail.com>
2025-02-11 21:37:17 -0500

    bug fix

--- app/wallet/[address]/page.tsx
+++ app/wallet/[address]/page.tsx
@@ -9,8 +9,9 @@ interface WalletPageProps {
     };
 }
 
-const WalletPage: React.FC<WalletPageProps> = async ({ params }) => {
-    const { address } = await params;
+const WalletPage = async ({ params }: WalletPageProps) => {
+    // No need to await params since it's not a promise
+    const { address } = params;
     return <Wallet address={address} />;
 };
 
