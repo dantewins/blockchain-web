06238d2a0b3efc00f246e3fd15c7772be68ae5f3
Azurium-js <kimdanny0603@gmail.com>
2025-02-11 21:42:52 -0500

    redo again

--- app/wallet/[address]/page.tsx
+++ app/wallet/[address]/page.tsx
@@ -4,15 +4,14 @@ import React from 'react';
 import Wallet from '@/components/wallet';
 
 interface WalletPageProps {
-    params: {
-        address: string;
-    };
+  params: {
+    address: string;
+  };
 }
 
-const WalletPage = async ({ params }: WalletPageProps) => {
-    // No need to await params since it's not a promise
-    const { address } = params;
-    return <Wallet address={address} />;
+const WalletPage = ({ params }: WalletPageProps) => {
+  const { address } = params;
+  return <Wallet address={address} />;
 };
 
 export default WalletPage;
